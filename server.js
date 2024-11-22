b8627510b1cab80383af64036fdc57c3a22ed385
super-dev <super643@outlook.com>
2024-11-22 06:15:18 -0800

    updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
